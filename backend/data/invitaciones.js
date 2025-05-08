07ff19c1879274512a1884759bfc5be0d1760099
Rodri <rodrigomagallanes0@gmail.com>
2025-05-08 01:41:48 -0300

    Se modifico el back para que funcione unos comandos

---
+++ backend/data/invitaciones.js
@@ -0,0 +1,7 @@
+module.exports = {
+    Invitaciones: [
+        { id: 4, materia: 'Arquitectura', titular: 'Gilda', ayudante: '', fecha: '2025-06-10', alumnos: [], sugerido: 'Carlos' },
+        { id: 5, materia: 'Algoritmos', titular: '', ayudante: 'Jose', fecha: '2025-06-15', alumnos: [], sugerido: 'Figue' }
+    ]
+  
+  };
