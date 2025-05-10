c74d43738dbff13b600770a1248c9a3fef870001
Joaco981 <joaquinnfloress16@gmail.com>
2025-05-10 19:01:39 -0300

    cambios generales para empezar a usar PWA con Vue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,40 @@
 // vite.config.js (CommonJS)
+import { VitePWA } from 'vite-plugin-pwa';
 const { defineConfig } = require('vite')
 const vue = require('@vitejs/plugin-vue')
 
 module.exports = defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), 
+    VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Mi PWA',
+                short_name: 'PWA',
+                description: 'Una aplicación web progresiva con Vue y Vite',
+                theme_color: '#4DBA87',
+                icons: [
+                  {
+                        src: '/icons/144.png',
+                        sizes: '144x144',
+                        type: 'image/png',
+                    },  
+                  {
+                        src: '/icons/512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/icons/1024.png',
+                        sizes: '1024x1024',
+                        type: 'image/png',
+                    },
+                ],
+                start_url: '/',
+                display: 'standalone',
+                background_color: '#ffffff',
+            },
+        }),
+  ],
   test: {
     environment: 'jsdom',
     coverage: {
